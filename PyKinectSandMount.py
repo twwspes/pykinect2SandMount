3eb1bd26c7b62e10e69cb321bacf7476352973ef
unknown <spes@connect.hku.hk>
2019-10-23 10:18:50 +0800

    alignment has been hardcoded, colours of topographic has been adjusted

--- PyKinectSandMount.py
+++ PyKinectSandMount.py
@@ -26,13 +26,14 @@ SKELETON_COLORS = [pygame.color.THECOLORS["red"],
 
 class SandMountRuntime(object):
 
-    didBackgroundDepthSaved = False
+    didBackgroundDepthSaved = True
     didBackgroundDepthLoaded = False
     arrayOfBackgroundDepth = []
-    limitedOffsetX = 86
-    limitedOffsetY = 110
-    limitedWidth = 296
-    limitedHeight = 165
+    arrayOfWaterDrop = []
+    limitedOffsetX = 54
+    limitedOffsetY = 63
+    limitedWidth = 336
+    limitedHeight = 189
 
     def __init__(self):
         pygame.init()
@@ -47,14 +48,17 @@ class SandMountRuntime(object):
         self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Depth | PyKinectV2.FrameSourceTypes_Color)
 
         # back buffer surface for getting Kinect infrared frames, 8bit grey, width and height equal to the Kinect color frame size
-        self._frame_surface = pygame.Surface((self.limitedWidth, self.limitedHeight), 0, 24)
+        # self._frame_surface = pygame.Surface((self.limitedWidth, self.limitedHeight), 0, 24)
+        self._frame_surface = pygame.Surface((int(1920/self.limitedWidth)*self.limitedWidth, int(1080/self.limitedHeight)*self.limitedHeight), 0, 24)
         # here we will store skeleton data 
         self._bodies = None
         
         # Set the width and height of the screen [width, height]
         self._infoObject = pygame.display.Info()
-        self._screen = pygame.display.set_mode((self.limitedWidth, self.limitedHeight), 
-                                                pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
+        self._screen = pygame.display.set_mode((int(1920/self.limitedWidth)*self.limitedWidth, int(1080/self.limitedHeight)*self.limitedHeight), 
+                                                pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN, 32)
+        # self._screen = pygame.display.set_mode((self.limitedWidth, self.limitedHeight), 
+        #                                         pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, 32)
 
         pygame.display.set_caption("Kinect for Windows v2 SandMount")
 
@@ -95,18 +99,28 @@ class SandMountRuntime(object):
         self.load_depth_frame()
         target_surface.lock()
         # get an array of heights of any objects on top of the background, and flipping its left and right sides
-        objectHeights = self.arrayOfBackgroundDepth - frame
+        objectHeights = self.arrayOfBackgroundDepth - frame + 70
         objectHeights = np.reshape(objectHeights, (self._kinect.depth_frame_desc.Height, self._kinect.depth_frame_desc.Width))
         objectHeights = np.fliplr(objectHeights)
         objectHeights = objectHeights[self.limitedOffsetY:(self.limitedOffsetY + self.limitedHeight), self.limitedOffsetX:(self.limitedOffsetX + self.limitedWidth)]
-        # if height between 0 100 then from green to yellow, if height between 100 150 then from yellow to red
-        f8Red = np.uint8((objectHeights * 2.5).clip(0,250))
-        f8Red = np.reshape(f8Red, (self.limitedHeight * self.limitedWidth, ))
-        f8Green = np.uint8((objectHeights * -3 + 750).clip(0,250))
-        f8Green = np.reshape(f8Green, (self.limitedHeight * self.limitedWidth, ))
+        # if height between 0 50 then from green to yellow, if height between 50 100 then from yellow to red
+        f8Red = np.uint8((objectHeights * 50/18).clip(0,250))
+        f8Green = np.uint8((objectHeights * -3 + 500).clip(0,250))
         f8Blue = np.uint8((objectHeights.clip(0,1)))
-        f8Blue = np.reshape(f8Blue, (self.limitedHeight * self.limitedWidth,))
-        frame8bit = np.dstack((f8Blue, f8Green, f8Red))
+        f8Redfloat = np.kron(f8Red, np.ones((int(1080/self.limitedHeight), int(1920/self.limitedWidth))))
+        f8Redint = f8Redfloat.astype(np.uint8)
+        f8Greenfloat = np.kron(f8Green, np.ones((int(1080/self.limitedHeight), int(1920/self.limitedWidth))))
+        f8Greenint = f8Greenfloat.astype(np.uint8)
+        f8Bluefloat = np.kron(f8Blue, np.ones((int(1080/self.limitedHeight), int(1920/self.limitedWidth))))
+        f8Blueint = f8Bluefloat.astype(np.uint8)
+        f8Redint = np.reshape(f8Redint, (int(1080/self.limitedHeight)* self.limitedHeight * int(1920/self.limitedWidth) * self.limitedWidth, ))
+        f8Greenint = np.reshape(f8Greenint, (int(1080/self.limitedHeight)* self.limitedHeight * int(1920/self.limitedWidth) * self.limitedWidth, ))
+        f8Blueint = np.reshape(f8Blueint, (int(1080/self.limitedHeight)* self.limitedHeight * int(1920/self.limitedWidth) * self.limitedWidth, ))
+        frame8bit = np.dstack((f8Blueint, f8Greenint, f8Redint))
+        # f8Red = np.reshape(f8Red, (self.limitedHeight * self.limitedWidth, ))
+        # f8Green = np.reshape(f8Green, (self.limitedHeight * self.limitedWidth, ))
+        # f8Blue = np.reshape(f8Blue, (self.limitedHeight * self.limitedWidth,))
+        # frame8bit = np.dstack((f8Blue, f8Green, f8Red))
         address = self._kinect.surface_as_array(target_surface.get_buffer())
         ctypes.memmove(address, frame8bit.ctypes.data, frame8bit.size)
         del address
@@ -120,6 +134,15 @@ class SandMountRuntime(object):
             for event in pygame.event.get(): # User did something
                 if event.type == pygame.QUIT: # If user clicked close
                     self._done = True # Flag that we are done so we exit this loop
+
+                if event.type == pygame.KEYDOWN: # If user press Keyboard
+                    if event.key == pygame.K_q: # If user press Keyboard q
+                        #self._screen = pygame.display.set_mode((1920, 1080), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN, 32)
+                        self._done = True # Flag that we are done so we exit this loop
+                    if event.key == pygame.K_f: # If user press Keyboard f
+                        self._screen = pygame.display.set_mode((1920, 1080), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN, 32)
+                    if event.key == pygame.K_a: # If user press Keyboard a
+                        self.didBackgroundDepthSaved = False
 
                 elif event.type == pygame.VIDEORESIZE: # window resized
                     self._screen = pygame.display.set_mode(event.dict['size'], 
